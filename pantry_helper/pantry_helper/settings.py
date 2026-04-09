32d80671400001998c4b082625dd0b30578ddfe9
AnaSL-WRK <ana.wk.lsl@gmail.com>
2026-04-09 23:59:54 +0100

    pythonanywhere

--- pantry_helper/pantry_helper/settings.py
+++ pantry_helper/pantry_helper/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9#_e(!5x23#13)aygzla375uupb894*)jzgip&5um(+#66f2q3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['anawk25.pythonanywhere.com']
 
 
 # Application definition
@@ -120,7 +120,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
